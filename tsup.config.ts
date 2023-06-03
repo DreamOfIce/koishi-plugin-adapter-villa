4298e98450b7f7d2a0381c2391d49744d9b9effe
DreamOfIce <dreamofice@outlook.com>
2023-06-03 17:52:14 +0800

    chore: update build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
+  outDir: "lib",
   dts: true,
+  sourcemap: true,
   clean: true,
 });
