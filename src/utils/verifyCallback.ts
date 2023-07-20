188853501d8634e22f10fb8a150a540e6d95d4d6
DreamOfIce <dreamofice@outlook.com>
2023-07-21 00:13:07 +0800

    feat: add public key import cache

--- src/utils/verifyCallback.ts
+++ src/utils/verifyCallback.ts
@@ -1,6 +1,9 @@
 import { webcrypto } from "crypto";
 import { base64ToArrayBuffer } from "koishi";
 
+// public key import cache
+const publicKeys: Record<string, webcrypto.CryptoKey> = {};
+
 export const verifyCallback = async (
   signature: string,
   secret: string,
@@ -15,13 +18,13 @@ export const verifyCallback = async (
     }).toString()
   );
 
-  const publicKey = await webcrypto.subtle.importKey(
+  const publicKey = (publicKeys[pubKey] ??= await webcrypto.subtle.importKey(
     "spki",
     base64ToArrayBuffer(pubKey.slice(26, -24)),
     { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" },
     false,
     ["verify"]
-  );
+  ));
   return await webcrypto.subtle.verify(
     "RSASSA-PKCS1-v1_5",
     publicKey,
